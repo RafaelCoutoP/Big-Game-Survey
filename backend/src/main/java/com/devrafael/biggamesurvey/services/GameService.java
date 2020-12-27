73639b77f9a5ddd49c08d78f73421eb0d5777722
rafaelc <rafaelcouto@hotmail.com>
2020-12-26 23:33:33 -0300

    implements GameDTO and GameService

---
+++ backend/src/main/java/com/devrafael/biggamesurvey/services/GameService.java
@@ -0,0 +1,25 @@
+package com.devrafael.biggamesurvey.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devrafael.biggamesurvey.dto.GameDTO;
+import com.devrafael.biggamesurvey.entities.Game;
+import com.devrafael.biggamesurvey.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<GameDTO> findAll(){
+		List<Game> list = repository.findAll();
+		return list.stream().map(x -> new GameDTO(x)).collect(Collectors.toList());
+	}
+}
